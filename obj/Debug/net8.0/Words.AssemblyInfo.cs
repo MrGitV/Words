a52e6eb95a4ef7ee8bcec8ab564ee4ae551b3ed9
MrGitV <142344641+MrGitV@users.noreply.github.com>
2025-03-19 22:23:09 +0300

    Вправка

--- obj/Debug/net8.0/Words.AssemblyInfo.cs
+++ obj/Debug/net8.0/Words.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Words")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1087d079c80e86b41ad378152565b5580e89aea4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+927b5fa356e5c78ee72f0834310cbeea58967a58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Words")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Words")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
